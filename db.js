103490d6c099bd9dbd1f984c9badd86ea95f6f15
Marcos Molina <210597@utags.edu.mx>
2025-12-06 17:40:36 -0600

    Mongo URI apunta a /test y usa authSource=admin para evitar fallos de conexión

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb://mongo:rxRcvNBKAuYrnltEtALpONsgPuGrqdBb@crossover.proxy.rlwy.net:21591';
+const MONGO_URI = 'mongodb://mongo:rxRcvNBKAuYrnltEtALpONsgPuGrqdBb@crossover.proxy.rlwy.net:21591/test?authSource=admin';
 
 async function connectDB() {
   try {
